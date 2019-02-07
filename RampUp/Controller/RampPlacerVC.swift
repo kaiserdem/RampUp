5cb96996b51b2ef6c71e16cf77f466c1426557fb
Yaroslav Golinskiy <kaiserdem@gmail.com>
2019-02-07 15:53:32 +0200

    create PampPickerVC

--- RampUp/Controller/RampPlacerVC.swift
+++ RampUp/Controller/RampPlacerVC.swift
@@ -10,7 +10,7 @@ import UIKit
 import SceneKit
 import ARKit
 
-class RampPlacerVC: UIViewController, ARSCNViewDelegate {
+class RampPlacerVC: UIViewController, ARSCNViewDelegate, UIPopoverPresentationControllerDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
@@ -23,7 +23,7 @@ class RampPlacerVC: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene(named: "art.scnassets/pipe.dae")!
         
         // Set the scene to the view
         sceneView.scene = scene
@@ -68,7 +68,19 @@ class RampPlacerVC: UIViewController, ARSCNViewDelegate {
     }
     
     func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+      
     }
+  func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+    return .none
+  }
+  
+  @IBAction func onRampBtnPressed(_ sender: UIButton) {
+    let rampPickerVC = PampPickerVC(size: CGSize(width: 250, height: 500))
+    rampPickerVC.modalPresentationStyle = .popover
+    rampPickerVC.popoverPresentationController?.delegate = self
+    present(rampPickerVC, animated: true, completion: nil)
+    rampPickerVC.popoverPresentationController?.sourceView = sender
+    
+  }
+  
 }
